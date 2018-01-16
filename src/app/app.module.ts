9fd816fee5f5b6cbf8310cb08d4f2cc30849a73a
mile1983 <milexic@gmail.com>
2018-01-16 19:24:11 +0100

    1 zadatak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
