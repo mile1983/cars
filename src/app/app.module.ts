9d7cc735a35f1af07f81c455734180c556a8854e
mile1983 <milexic@gmail.com>
2018-01-16 20:41:41 +0100

    2 komit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './components/cars/cars.component';
 import { LayoutComponent } from './components/layout/layout.component';
+
+const appRoutes: Routes = [
+  {
+    path:'',
+    redirectTo: '/cars',
+    pathMatch:'full'
+  },
+  {
+    path:'cars',
+    component: CarsComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
-    LayoutComponent
+    LayoutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
